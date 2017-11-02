e9338d98d017b125bc81525c148a3f9814053c19
Sean Ruhland <sean.ruhland@gmail.com>
2017-11-01 17:55:21 -0700

    dummy data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PlayerViewer from './playerViewer.js'
+import PlayerViewer from './playerViewer.js';
+import samplePlayers from './fantasy-players';
+
+console.log(samplePlayers);
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.loadPlayers = this.loadPlayers.bind(this);
+    //initial state
+    this.state = {
+      players: {},
+      order: {}
+    };
+  }
+
+  loadPlayers() {
+    this.setState({
+      players: samplePlayers
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,11 +33,11 @@ class App extends Component {
         </header>
         <div>
           <p className="App-intro">We are going to make an App to help us win fantasy basketball</p>
-          <playerViewer />
+          <PlayerViewer loadPlayers= {this.loadPlayers} className="PlayerView"/>
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default App;
--- src/playerViewer.js
+++ src/playerViewer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-
 class PlayerViewer extends React.Component {
   render() {
     return(
         <div className="playerView">
           <h2>Players, I want this to show up!</h2>
+          <button onClick={this.props.loadPlayers}> Load Players </button>
         </div>
       )
   }
 }
+
 export default PlayerViewer;
