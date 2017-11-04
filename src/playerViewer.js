c9d85b760756eb6f3221541b930820d49d84e18b
Sean Ruhland <sean.ruhland@gmail.com>
2017-11-03 21:30:40 -0700

    WIP Bootstrap grid

--- src/playerViewer.js
+++ src/playerViewer.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import Players from './fantasy-players'
+import './App.css';
+
 
 function PlayersList(props) {
   const players = props.players
   const listItems = players.map((player) =>
-        <div key={player.key.toString()}>
-            <div> {player.NAME}</div>
-            <div>Projected Points : {player.PROJ_PTS} Total Games this week: {player.TOTAL}</div>
-            <div>15 day average: {player.AVG15} 30 day average: {player.AVG30}</div>
+        <div class="container" key={player.key.toString()}>
+            <div class="Name"> {player.NAME}</div>
+             <div class="row">
+                 <div class="rr rr-left" class="col-md-auto">
+                  Total Games this week: {player.TOTAL}
+                </div>
+                <div class="rr rr-right" class="col-md-auto">
+                  Projected Points : {player.PROJ_PTS}
+                </div>
+             </div>
+             <div class="row">
+                 <div class=".col-6 .col-md-4">
+                  7 day average:       {player.AVG7}
+                </div>
+                <div class=".col-6 .col-md-4">
+                  15 day average: {player.AVG15}
+                </div>
+                 <div class=".col-6 .col-md-4">
+                  30 day average: {player.AVG30}
+                </div>
+               </div>
+             <div class="row">
+                <div class="col" class="Season">
+                  Season Average
+                </div>
+             </div>
+             <div class="row">
+                <div class="col" class="Season">
+                  {player.AVGS}
+                </div>
+             </div>
           </div>
 
   );
@@ -21,6 +50,10 @@ function PlayersList(props) {
   
   const players = Players;
 class PlayerViewer extends React.Component {
+  constructor(){
+    super();
+    this.state = {players}
+  }
   render() {
     return(
         <div className="playerView">
