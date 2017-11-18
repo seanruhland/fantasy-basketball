26890f9ea3b8243b93132e644f9498a653e91e39
Sean Ruhland <sean.ruhland@gmail.com>
2017-11-17 23:04:52 -0800

    WIP photoView

--- src/playerViewer.js
+++ src/playerViewer.js
@@ -2,46 +2,31 @@ import React from 'react';
 import Players from './fantasy-players'
 import './App.css';
 
+  const divStyle  =  {
+        width: 202, 
 
+};
 function PlayersList(props) {
   const players = props.players
   const listItems = players.map((player) =>
-        <div className="container" key={player.key.toString()}>
-            
-            <div className="align-items-center">
-              <div className="Name"> {player.NAME}</div>
+        <div key={player.key.toString()}>
+            <div class="card text-center" style={divStyle}>
+             <div class="card-body">
+                <h4 class="card-title">{player.name}</h4>
+                <h6 class="card-subtitle text-muted">Average: {player.AVGS} Projected Points: {player.PROJ_PTS}</h6>
               </div>
-             <div className="row">
-                 <div className="col-6">
-                  Total Games this week: {player.TOTAL}
-                </div>
-                <div className="col-6">
-                  Projected Points : {player.PROJ_PTS}
-                </div>
-             </div>
-             <div className="row">
-                 <div className="col-md-auto">
-                  7 day average:       {player.AVG7}
-                </div>
-                <div className="col-md-auto">
-                  15 day average: {player.AVG15}
-                </div>
-                 <div className="col-md-auto">
-                  30 day average: {player.AVG30}
-                </div>
-               </div>
-             <div className="row">
-                <div className="col" className="Season">
-                  Season Average
-                </div>
-             </div>
-             <div className="row">
-                <div className="col" className="Season">
-                  {player.AVGS}
-                </div>
-             </div>
-          </div>
 
+
+                  <img src="https://cdn.fansided.com/wp-content/assets/site_images/fansided/headers/logo-nba-free-agency__150x150.png" alt="Photo of sunset" />
+              <div class="card-body">
+                <p class="card-text">7 day average: {player.AVG7}</p>
+                <p class="card-text">15 day average: {player.AVG15}</p>
+                <p class="card-text">30 day average: {player.AVG30}</p>
+
+                <a href="#" class="btn btn-info">Full Report</a>
+              </div>
+            </div>
+          </div>
   );
   return (
     <ul>{listItems}</ul>
@@ -51,7 +36,7 @@ function PlayersList(props) {
 
 
   
-  const players = Players;
+  const players = Players.freeAgents;
 class PlayerViewer extends React.Component {
   constructor(){
     super();
--- src/teamView.js
+++ src/teamView.js
@@ -4,34 +4,27 @@ import './App.css';
 
 // console.log(Data.Team)
 
+  const divStyle  =  {
+  			width: 500,	
+
+};
 function TeamView(props) {
   const teams = props.data
-   // console.log(teams)
   const listItems = teams.map((team) =>
-        <div className="container" key={team.key.toString()}>
-            <div class="card">
-				<img class="playerImage" class="card-image-top" src="http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3975.png" alt="Stephen Curry" />
-				<div class="card-body">
-				<h4 class="card-title">{team.name}</h4>
-				<p class="card-text">{team.players.join(" ")}</p>
-				<a href="#" class="btn btn-primary">See Profile</a>
+        <div style={divStyle} key={team.key.toString()}>
+            <div classNAme="card" >
+				<img className="card-image-top" src="http://14042-presscdn-0-36.pagely.netdna-cdn.com/wp-content/uploads/2015/02/gzxolsnmemnno8jxwvbt.jpg" alt="Stephen Curry" />
+				<div className="card-body">
+					<h4 className="card-title">{team.name}</h4>
+					<p className="card-text">{team.players.join(" ")}</p>
+					<a href="#" className="btn btn-primary">More Stats</a>
+					</div>
 				</div>
-		</div>
-            <div className="card testimonial-card">
-				<div>
-            		<div className="card-body">
-				        <h4 className="card-title">{team.name}</h4>
-				        
-				        <p><i className="fa fa-quote-left"></i> {team.players.join(" ")}</p>
+           </div>
 
-    				</div>
-            	</div>
-            </div>
-            	
-          </div>
 
   );
-    console.log(listItems)
+
   return (
     <ul>{listItems}</ul>
   );
@@ -47,6 +40,7 @@ class TeamData extends React.Component {
     super();
     this.state = {data}
   }
+
   render() {
     return(
         <div className="playerView">
