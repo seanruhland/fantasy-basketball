16e3e6d85b6868c672d78cce1b5ed566f077f473
Sean Ruhland <sean.ruhland@gmail.com>
2019-01-21 12:00:18 -0800

    bball app

--- src/playerViewer.js
+++ src/playerViewer.js
@@ -7,19 +7,45 @@ function PlayersList(props) {
 
   const listItems = players.map((player) =>
         <div key={player.key.toString()}>
-            <div className="container">
-                <div className="row"><img className="imgSize" src={player.imgsrc}/>
+            <div className="container gradient">
+                <div className="row">
+                    <div className="cold-md-4">
+                      <h5 className="name">{player.name}</h5>
+                      <h7 className='position'>{player.team}/{player.position}</h7>
+                      <img className="imageOne" src={player.imgsrc}/>
+                      <img className="imageTwo" src="http://grantland.com/wp-content/themes/vip/espn-grantland/assets/parker_animation/images/court_small.png"/> 
+                    </div>
                     <div className="col-md-4 text">
-                      <h3 className="name">{player.name}</h3>
                       <br></br>
                       <h6>Total Games this Week: {player.TOTAL}</h6>
                       <h6> Projected Points: {Math.round(player.PROJ_PTS)}</h6>
                     </div>
                     <div className="col-md-4 text">
-                      <p>7 day average: {player.AVG7}</p>
-                      <p>15 day average: {player.AVG15}</p>
-                      <p>30 day average: {player.AVG30}</p>
-                      <p >Composite Avg: {Math.round(player.COMPOSITE)}</p>
+                      <table className="table table-condensed">
+                          <thead>
+                            <tr>
+                              <th id="averages" colspan="2">Averages</th>
+                              <th colspan="3"></th>
+
+                            </tr>
+                            <tr>
+                              <td>Season   </td>
+                              <td>last7    </td>
+                              <td>last15   </td>
+                              <td>last30   </td>
+                              <td id="averages">Composite</td>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td className="numbers">{player.AVGS}  </td>
+                              <td className="numbers">{player.AVG7}  </td>
+                              <td className="numbers">{player.AVG15}  </td>
+                              <td className="numbers">{player.AVG30}  </td>
+                              <td id="composite">{Math.round(player.COMPOSITE)}</td>
+                            </tr>
+                          </tbody>
+                        </table>
                     </div>
                 </div>
               </div>
